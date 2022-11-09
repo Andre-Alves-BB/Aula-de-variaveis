0b7e1c4214497f097eee413a63e79a90eb312bd6
André Alves Barbosa <andrealvesbarbosa@hotmail.com>
2022-11-08 21:58:44 -0300

    aula de variaveis

---
+++ aulaDeVariaveis.js
@@ -0,0 +1,14 @@
+// const é uma variavel imutavel.
+
+const cidade = "Uberaba, MG";
+
+console.log(cidade);
+
+// let ou var são variaveis mutaveis.
+
+let populacaoDaCidade = 337.092;
+console.log(populacaoDaCidade);
+
+//string templates sempre é utilizado crase ao fazer a linha de codigo.
+
+console.log(`minha cidade natal é ${cidade}, e contem ${populacaoDaCidade}`);
