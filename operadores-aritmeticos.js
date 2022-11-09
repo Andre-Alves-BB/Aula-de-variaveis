1904f90f591c10625eea3d881d216ea242938193
André Alves Barbosa <andrealvesbarbosa@hotmail.com>
2022-11-09 15:51:46 -0300

    operadores

---
+++ operadores-aritmeticos.js
@@ -0,0 +1,18 @@
+let x = 10, y = 2;
+
+let resultado = x + y;
+console.log(`A soma de ${x} e ${y} é ${resultado}`);
+
+resultado = x - y;
+
+console.log(`A subtração de ${x} e ${y} é ${resultado}`);
+
+resultado = x * y;
+console.log(`A multiplicação de ${x} e ${y} é ${resultado}`);
+
+resultado = x / y;
+console.log(`A divisão de ${x} e ${y} é ${resultado}`);
+
+
+// usei o "string template" pois era mais viavel e de melhor visibilidade.
+//poderia usar o "+" para concatenação.
