4241623cb43c7d5f90c8015ffc7af476260b4ad0
André Alves Barbosa <andrealvesbarbosa@hotmail.com>
2022-11-08 22:31:58 -0300

    Aula de Variaveis

--- aulaDeVariaveis.js
+++ aulaDeVariaveis.js
@@ -12,3 +12,21 @@ console.log(populacaoDaCidade);
 //string templates sempre é utilizado crase ao fazer a linha de codigo.
 
 console.log(`minha cidade natal é ${cidade}, e contem ${populacaoDaCidade}`);
+{
+    {
+        {
+            /*chaves é usado para delimitar uma area a se escrever determinado codigo,
+            então tudo que estiver descrito dentro do scopo sera de uso exclusivo daquela
+            area, ou seja se caso criar uma variavel dentro do scopo e tentar chamar ela fora 
+            isso não dara certo pois ela esta programada a ser utilizada somente dentro da area
+            delimitada dentro das chaves, mais caso eu crie uma variavel fora das chaves e 
+            chamar a mesma dentro das chaves isso ira funcionar normalmente, o que não posso fazer é utilizar 
+            o que esta dentro das chaves depois de fechalas, então se criar variaveis ou outras coisas antes de 
+            criar as chaves isso eu posso utlizar dentro das chaves*/
+
+        }
+    }
+
+
+
+}
